2af391194c846b787768b0062f5f6bc42d66413e
Kisjelica <kisjelicarastko@gmail.com>
2021-08-31 03:57:09 +0200

    Added dynamic search by category.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,27 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ProductService } from './services/product.service';
+
+import { Routes, RouterModule } from '@angular/router';
+import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
+const routes:Routes = [
+  //first match wins, define from most specific to most generic
+  {path:'category/:id',component:ProductListComponent},//
+  {path:'category',component:ProductListComponent},//default id
+  {path:'products',component:ProductListComponent},//default id
+  {path:'',redirectTo:'/products',pathMatch:'full'},//no specified path
+  {path:'**',redirectTo:'/products',pathMatch:'full'}//anything else - generic wildcard
+
+
+]
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ProductCategoryMenuComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
     HttpClientModule
   ],
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -3,25 +3,43 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Product } from '../common/product';
 import { map } from 'rxjs/operators';
+import { ProductCategory } from '../common/product-category';
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+  
 
-  private baseUrl = "http://localhost:8080/api/products?size=100";
+  private baseUrl = "http://localhost:8080/api/products";
   constructor(private httpClient:HttpClient) { }
+  private categoryUrl = "http://localhost:8080/api/product-category";
+  getProductList(categoryId: number):Observable<Product[]>{
+    //build url based on category id
 
-  getProductList():Observable<Product[]>{
-    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${categoryId}`;
+
+    return this.httpClient.get<GetResponseProduct>(searchUrl).pipe(
       map(response => response._embedded.products)
     );
   }
 
+  getProductCategories():Observable<ProductCategory[]> {
+  
+    return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
+      map(response => response._embedded.productCategory)
+    );
+  }
   
 }
 //unwraps the JSON from the Spring Data REST _embedded entry
-interface GetResponse{
+interface GetResponseProduct{
   _embedded:{
     products:Product[];
+  }
+}
+
+interface GetResponseProductCategory{
+  _embedded:{
+    productCategory:ProductCategory[];
   }
 }
